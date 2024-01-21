d90decf50039f5b059ebd202d463d06ec8434fa7
Stefan Novak <stefannovak96@gmail.com>
2024-01-21 13:19:29 +0000

    feat: allow Swagger on Prod

--- TaskDemo.Api/Program.cs
+++ TaskDemo.Api/Program.cs
@@ -6,14 +6,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 var summaries = new[]
